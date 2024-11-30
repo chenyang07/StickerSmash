36d8432c3990dedfe391b2011cbf3b8be30a4d5f
Anthony X <35902229+chenyang07@users.noreply.github.com>
2024-11-30 17:39:53 -0500

    .

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,7 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
     </View>
   );
